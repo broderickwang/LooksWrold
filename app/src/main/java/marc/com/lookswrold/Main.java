91f07f5e0cef1477a2e2e47768e8c2cd02569f2e
marc <wangchengda1990@gmail.com>
2016-09-14 16:38:57 +0800

    修改图标,转换圆形头像

--- app/src/main/java/marc/com/lookswrold/Main.java
+++ app/src/main/java/marc/com/lookswrold/Main.java
@@ -1,31 +1,33 @@
 package marc.com.lookswrold;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.List;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import marc.com.lookswrold.adapter.GitHubAdaptor;
-import marc.com.lookswrold.bean.Contributor;
-import marc.com.lookswrold.face.GitHubClient;
+import marc.com.lookswrold.bean.StartUser;
+import marc.com.lookswrold.face.GetZhihuService;
 import marc.com.lookswrold.fragement.GithubFragement;
 import marc.com.lookswrold.fragement.TestFragement;
 import marc.com.lookswrold.fragement.ZhihuFragement;
+import marc.com.lookswrold.util.GlideCircleTransform;
+import marc.com.lookswrold.util.GlideRoundTransform;
 import retrofit.Call;
 import retrofit.Callback;
 import retrofit.GsonConverterFactory;
@@ -49,6 +51,8 @@ public class Main extends AppCompatActivity
 	GithubFragement githubFragement;
 	ZhihuFragement zhihuFragement;
 	ActionBarDrawerToggle mDrawerToggle;
+	ImageView head_tx;
+	TextView head_name;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -57,16 +61,17 @@ public class Main extends AppCompatActivity
 		ButterKnife.bind(this);
 
 		toolbar.setTitle("看天下");
-//		toolbar.setSubtitle("");
+		toolbar.setTitleTextColor(Color.WHITE);
 		toolbar.inflateMenu(R.menu.app_menu);
 		setSupportActionBar(toolbar);
-
-
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		mDrawerToggle = new ActionBarDrawerToggle(this,drawLayout,toolbar,
 				R.string.drawer_open, R.string.drawer_close);
 		mDrawerToggle.syncState();
 		drawLayout.setDrawerListener(mDrawerToggle);
+		//navigationicon需要最后设置,不然无效
+//		toolbar.setOnMenuItemClickListener();
+		toolbar.setNavigationIcon(R.drawable.menu);
 
 		navigation.setNavigationItemSelectedListener(this);
 		initView();
@@ -88,6 +93,47 @@ public class Main extends AppCompatActivity
 		testFragement = new TestFragement();
 		githubFragement = new GithubFragement();
 		zhihuFragement = new ZhihuFragement();
+		getPhoto();
+	}
+	public void getPhoto() {
+
+		//获取navigation中的控件
+		View view = navigation.getHeaderView(0);
+		head_tx = (ImageView)view.findViewById(R.id.head_tx);
+		head_name = (TextView)view.findViewById(R.id.head_name);
+
+		Retrofit retrofit = new Retrofit.Builder()
+				.baseUrl(ZhihuFragement.BASE_URL)
+				.addConverterFactory(GsonConverterFactory.create())
+				.build();
+
+		GetZhihuService github = retrofit.create(GetZhihuService.class);
+		Call<StartUser> call = github.getSplash("1080*1776");
+
+		call.enqueue(new Callback<StartUser>() {
+			@Override
+			public void onResponse(Response<StartUser> response, Retrofit retrofit) {
+				if(response.body() != null) {
+					Glide.with(Main.this)
+							.load(response.body().getImg())
+							.centerCrop()
+							.diskCacheStrategy(DiskCacheStrategy.ALL)
+							.transform(new GlideCircleTransform(Main.this))
+							.placeholder(R.drawable.home)
+							.error(R.drawable.zombie)
+							.crossFade()
+							.into(head_tx);
+					head_name.setText(response.body().getText());
+					Log.i("Tag", "onResponse: "+response.body().getText());
+
+				}
+			}
+
+			@Override
+			public void onFailure(Throwable t) {
+
+			}
+		});
 	}
 
 	@Override
